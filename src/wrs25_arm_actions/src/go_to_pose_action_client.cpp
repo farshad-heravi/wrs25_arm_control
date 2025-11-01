711e1804244b6091b1d32e18f0b21cf00258ab34
farshad-heravi <f.n.heravi@gmail.com>
2025-11-01 19:32:14 +0100

    - implemented pose marker publishing in GoToPoseActionClient (for debugging purposes) - Added visualization and tf2 dependencies;

--- src/wrs25_arm_actions/src/go_to_pose_action_client.cpp
+++ src/wrs25_arm_actions/src/go_to_pose_action_client.cpp
@@ -4,6 +4,10 @@
 #include "wrs25_arm_actions/action/go_to_pose.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 
+#include <visualization_msgs/msg/marker.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+
 class GoToPoseActionClient : public rclcpp::Node
 {
 public:
@@ -14,6 +18,12 @@ public:
         : Node("go_to_pose_action_client", node_options)
     {
         this->client_ptr_ = rclcpp_action::create_client<GoToPose>(this, "go_to_pose");
+
+        // add publisher for target pose marker
+        rclcpp::QoS qos(10);
+        qos.transient_local();
+        qos.reliable();
+        marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("target_pose_marker", qos);
     }
 
     void send_goal(const geometry_msgs::msg::PoseStamped & target_pose)
@@ -38,10 +48,85 @@ public:
             std::bind(&GoToPoseActionClient::result_callback, this, std::placeholders::_1);
         
         this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+        publish_marker(target_pose);
     }
 
 private:
+    std::string target_frame_id = "ur5_base_link";
+
     rclcpp_action::Client<GoToPose>::SharedPtr client_ptr_;
+
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
+
+    void publish_marker(const geometry_msgs::msg::PoseStamped & pose)
+    {
+        std::array<std::string, 3> axis_names = {"x_axis", "y_axis", "z_axis"};
+        std::array<std::array<float, 3>, 3> axis_colors = {{
+            {1.0, 0.0, 0.0},  // X = Red
+            {0.0, 1.0, 0.0},  // Y = Green
+            {0.0, 0.0, 1.0}   // Z = Blue
+        }};
+        
+        // Create quaternion from pose orientation
+        tf2::Quaternion quat(
+            pose.pose.orientation.x,
+            pose.pose.orientation.y,
+            pose.pose.orientation.z,
+            pose.pose.orientation.w
+        );
+        
+        // Convert quaternion to rotation matrix
+        tf2::Matrix3x3 rotation_matrix(quat);
+        
+        // Define base axis directions (in local frame)
+        std::array<tf2::Vector3, 3> base_axes = {
+            tf2::Vector3(1.0, 0.0, 0.0),  // X
+            tf2::Vector3(0.0, 1.0, 0.0),  // Y
+            tf2::Vector3(0.0, 0.0, 1.0)   // Z
+        };
+        
+        // Rotate axes according to pose orientation
+        std::array<tf2::Vector3, 3> rotated_axes;
+        for (size_t i = 0; i < 3; i++)
+        {
+            rotated_axes[i] = rotation_matrix * base_axes[i];
+        }
+
+        for (size_t j = 0; j < 10; j++)
+        {
+            for (size_t i = 0; i < 3; i++)
+            {
+                visualization_msgs::msg::Marker marker;
+                marker.header.frame_id = target_frame_id;
+                marker.header.stamp = this->get_clock()->now();
+                marker.ns = "";
+                marker.id = i;
+                marker.type = visualization_msgs::msg::Marker::ARROW;
+                marker.action = visualization_msgs::msg::Marker::ADD;
+
+                // Start at pose position, end along rotated axis direction
+                marker.points.resize(2);
+                marker.points[0] = pose.pose.position;
+                marker.points[1].x = pose.pose.position.x + rotated_axes[i].x() * 0.2;
+                marker.points[1].y = pose.pose.position.y + rotated_axes[i].y() * 0.2;
+                marker.points[1].z = pose.pose.position.z + rotated_axes[i].z() * 0.2;
+
+                marker.scale.x = 0.01;  // shaft diameter
+                marker.scale.y = 0.02;  // head diameter
+                marker.scale.z = 0.03;  // head length
+
+                marker.color.r = axis_colors[i][0];
+                marker.color.g = axis_colors[i][1];
+                marker.color.b = axis_colors[i][2];
+                marker.color.a = 1.0;
+
+                marker.lifetime = rclcpp::Duration(0, 0);
+                
+                marker_pub_->publish(marker);
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
 
     void goal_response_callback(const GoalHandleGoToPose::SharedPtr & goal_handle)
     {
